7d19bc7158ee2e10d4983adf6bed79ba66ad00ff
Darina <to.darina.to>
2021-03-11 19:09:07 +0200

    print message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Hello World!");
-	System.out.println("Hi mates!");
+
+    }
+    public static  void Message () {
+        System.out.println("Hi mates!");
     }
 }
